f4a3e35740cd319af4899bb378cc38f81e0bab5a
ProG-bot-ops <giangndhe181711@fpt.edu.vn>
2025-05-20 02:48:59 +0700

    fix bug

---
+++ myfile/bai1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a;
+a = int.TryParse(Console.ReadLine(), out a) ? a : 0;
+Console.WriteLine(a);
